2c7504b94df02e9d1e7f2e1f09b514501548bab0
Oleksandr Kozlov <9461015+alex-255@users.noreply.github.com>
2025-03-19 16:23:42 +0100

    mobile menu code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import SectionTeam from "./components/SectionTeam";
@@ -13,6 +13,27 @@ function App() {
     event.preventDefault();
     setPath(event.target.hash);
   };
+
+  useEffect(() => {
+    var menuBar = document.querySelector(".menu-bar");
+    // for mobile menu
+    var menuHamburger = document.querySelector(".menu-bar-hamburger");
+    menuHamburger.onclick = function () {
+      menuHamburger.classList.toggle("menu-opened");
+      menuBar.classList.toggle("menu-opened");
+    };
+
+    // for mobile menu closing while click on menu link
+    var menuLink = document.getElementsByClassName("menu-area-link");
+    var menuLinkLength = menuLink.length;
+
+    for (let step = 0; step < menuLinkLength; step++) {
+      menuLink[step].onclick = function () {
+        menuHamburger.classList.remove("menu-opened");
+        menuBar.classList.remove("menu-opened");
+      };
+    }
+  }, [path]);
 
   return (
     <>
